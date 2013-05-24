b9182912f2f0ef45ef84c21d28090d2c75630648
laci37 <laci37@gmail.com>
2013-05-24 17:48:03 +0200

    DMatrix injector methods

--- src/main/scala/smat/DMatrix.scala
+++ src/main/scala/smat/DMatrix.scala
@@ -5,10 +5,20 @@ import colt._
 //trait DMatrix extends Matrix[Double]
 
 object DMatrix{
+  
   val factory = DoubleFactory2D.dense
   import factory._
-  def zeros(r:Int, c:Int) = new DoubleMatrix(make(r,c))
-  def fill(r:Int, c:Int, value:Double) = new DoubleMatrix(make(r,c,value))
-  def eye(d:Int) = new DoubleMatrix(identity(d))
+  
+  def zeros(r: Int, c: Int) = new DoubleMatrix(make(r,c))
+  
+  def fill(r: Int, c: Int, value: => Double) = new DoubleMatrix(make(r,c,value))
+  
+  def eye(d: Int) = new DoubleMatrix(identity(d))
+  
+  def tabulate(r: Int, c: Int)(f: (Int,Int) => Double) = {
+    val arr = Array.tabulate(r,c){ (x,y) => f(x,y) }
+    new DoubleMatrix(make(arr))
+  }
+
 }
 
